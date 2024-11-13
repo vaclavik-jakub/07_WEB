fe14082c769f37debe0d686288d5288ab5ad3a8e
Jakub Václavík <jakub.vaclavik.022@pslib.cz>
2024-11-13 12:06:45 +0100

    nechapu, ale asi to funguje

--- 07_WEB/Program.cs
+++ 07_WEB/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using _07_WEB.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnections"))
+);
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(opt =>
+{
+    opt.Password.RequiredLength = 6;
+    opt.User.RequireUniqueEmail = true;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireDigit = false;
+    opt.SignIn.RequireConfirmedEmail = false;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthorization(c =>
+{
+    c.AddPolicy("Admin", p => p.RequireRole("Admin"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +40,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGroup("/api").MapCustomIdentityApi<IdentityUser>();
 
 app.Run();
